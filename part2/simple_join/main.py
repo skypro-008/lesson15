4b1bde87c74e6fb26091bafe53f3e6405dd718c8
lisvv <81390205+lisvv@users.noreply.github.com>
2022-05-26 20:40:59 +0300

    lesson15 part2

---
+++ part2/simple_join/main.py
@@ -0,0 +1,20 @@
+# Используя JOIN выведите все песни и название альбомов группы
+# Red Hot Chili Peppers, содержащиеся в базе.
+# Ознакомится со схемой базы данных можно в файле db_schema.png
+
+import sqlite3
+import prettytable
+
+con = sqlite3.connect("../music.db")
+cur = con.cursor()
+sqlite_query = ("")  # TODO составьте запрос на создание таблицы
+
+
+# Не удаляйте код ниже, он используется
+# для вывода заголовков созданной таблицы
+table = cur.execute(sqlite_query)
+mytable = prettytable.from_db_cursor(table)
+mytable.max_width = 30
+
+if __name__ == "__main__":
+    print(mytable)
